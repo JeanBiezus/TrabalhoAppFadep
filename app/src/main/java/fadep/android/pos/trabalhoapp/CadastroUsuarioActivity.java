2deb631c9a991345b821c756ecb96552530f32ef
Jean Biezus <jeancfbiezus@gmail.com>
2018-03-15 12:47:25 -0300

    first commit

---
+++ app/src/main/java/fadep/android/pos/trabalhoapp/CadastroUsuarioActivity.java
@@ -0,0 +1,82 @@
+package fadep.android.pos.trabalhoapp;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by jean on 15/03/18.
+ */
+
+public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_usuario_activity);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        //MOSTRA O NOME DO APP E O BOTÃO DE VOLTAR NA TOOLBAR
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void abrirCamera(View view) {
+        //INSTANCIA O PACKAGEMANAGER PARA PEGAR O CAMINHO DOS ARQUIVOS DO APP
+        PackageManager m = getPackageManager();
+        String s = getPackageName();
+        try {
+            PackageInfo p = m.getPackageInfo(s, 0);
+            s = p.applicationInfo.dataDir;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.w("yourtag", "Error Package name not found ", e);
+        }
+
+        //ABRE A CÂMERA
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.withAppendedPath(Uri.parse(s), "profile_img"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 1);
+        }
+    }
+
+
+    //AGUARDA O RETORNO DA IMAGEM
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+
+            //PEGA A IMAGEM TIRADA
+            Bitmap thumbnail = data.getParcelableExtra("data");
+
+
+            //PEGA A REFERÊNCIA DO IMAGEVIEW DO PERFIL E SETA O BACKGROUND COM A IMAGEM TIRADA
+            ImageView imageView = (ImageView) findViewById(R.id.profile_image);
+            imageView.setImageBitmap(thumbnail);
+        }
+    }
+}
---
+++ app/src/main/java/fadep/android/pos/trabalhoapp/LoginActivity.java
@@ -0,0 +1,34 @@
+package fadep.android.pos.trabalhoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by jean on 14/03/18.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_login);
+        setSupportActionBar(myToolbar);
+
+        //MOSTRA O NOME DO APP E O BOTÃO VOLTAR NO TOOLBAR
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        myToolbar.setTitle("");
+    }
+
+    //ABRE A ACTIVITY DE CADASTRO DE USUARIO
+    public void novoUsuario(View view) {
+        Intent intent = new Intent(this, CadastroUsuarioActivity.class);
+        startActivityForResult(intent, 1);
+    }
+}
